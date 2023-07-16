053721c6b2255a87ffa3953048a74800c429c3bd
Shane Myrick <shane@apollographql.com>
2023-07-16 12:17:08 -0700

    Get ready for blog release (#1)

--- dev.js
+++
@@ -1,9 +0,0 @@
-import { startSubgraphs } from "./subgraphs/subgraphs.js";
-
-// For local development, we will run a local router that will handle
-// composition and configure the ports of the router and subgraphs manually.
-// We could use `rover dev` here but that will not work for Stackblitz
-(async () => {
-  // start subgraphs in monolith mode
-  await startSubgraphs(4001);
-})();
--- subgraphs/trending/subgraph.js
+++ subgraphs/trending/subgraph.js
@@ -1,75 +1,14 @@
-import { ApolloServer } from "@apollo/server";
-import { expressMiddleware } from "@apollo/server/express4";
 import { buildSubgraphSchema } from "@apollo/subgraph";
-import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
-import { createServer } from "http";
-import express from "express";
-import { makeExecutableSchema } from "@graphql-tools/schema";
-import { WebSocketServer } from "ws";
-import { useServer } from "graphql-ws/lib/use/ws";
-import bodyParser from "body-parser";
-import cors from "cors";
 import { readFileSync } from "fs";
 import { parse } from "graphql";
 import { resolvers } from "./resolvers.js";
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
 
-const port = 4002;
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const typeDefs = parse(
   readFileSync(resolve(__dirname, "schema.graphql"), "utf8")
 );
 
-// Create the schema, which will be used separately by ApolloServer and
-// the WebSocket server.
-
-// const schema = makeExecutableSchema({ typeDefs, resolvers });
-const schema = buildSubgraphSchema({ typeDefs, resolvers });
-
-// Create an Express app and HTTP server; we will attach both the WebSocket
-// server and the ApolloServer to this HTTP server.
-const app = express();
-const httpServer = createServer(app);
-
-// Create our WebSocket server using the HTTP server we just set up.
-const wsServer = new WebSocketServer({
-  server: httpServer,
-  path: "/subscription",
-});
-// Save the returned server's info so we can shutdown this server later
-const serverCleanup = useServer({ schema }, wsServer);
-
-// Set up ApolloServer.
-const server = new ApolloServer({
-  schema,
-  plugins: [
-    // Proper shutdown for the HTTP server.
-    ApolloServerPluginDrainHttpServer({ httpServer }),
-
-    // Proper shutdown for the WebSocket server.
-    {
-      async serverWillStart() {
-        return {
-          async drainServer() {
-            await serverCleanup.dispose();
-          },
-        };
-      },
-    },
-  ],
-});
-
-await server.start();
-app.use("/graphql", cors(), bodyParser.json(), expressMiddleware(server));
-
-// Now that our HTTP server is fully set up, we can listen to it.
-httpServer.listen(port, () => {
-  console.log(
-    `Trending Server is now running on http://localhost:${port}/graphql`
-  );
-  console.log(
-    `Trending subscriptions now running on http://localhost:${port}/subscription`
-  );
-});
+export const getTrendingSchema = () =>
+  buildSubgraphSchema({ typeDefs, resolvers });
